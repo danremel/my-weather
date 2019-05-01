5cd85735ea531d1dcddd634a5d3d80b318f6b769
danremel <remel.dan@gmail.com>
2019-05-01 16:33:19 -0400

    Updates

---
+++ precache-manifest.37234247a8d4f73aa26625bf159b3e13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ecf7e11477d92865b6aa150d6da00c",
+    "url": "/my-weather/index.html"
+  },
+  {
+    "revision": "4d0686deda1b5def6138",
+    "url": "/my-weather/static/css/main.7381b289.chunk.css"
+  },
+  {
+    "revision": "39b70d17fcf7f5412aa8",
+    "url": "/my-weather/static/js/2.e57c843f.chunk.js"
+  },
+  {
+    "revision": "4d0686deda1b5def6138",
+    "url": "/my-weather/static/js/main.b1affacc.chunk.js"
+  },
+  {
+    "revision": "71373542fb7dfcf4e5f5",
+    "url": "/my-weather/static/js/runtime~main.37f04531.js"
+  }
+]);
